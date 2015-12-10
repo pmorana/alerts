83c4f6a02328c5e700fcf91592ad6bda3de2d067
pmorana <pmorana@gmail.com>
2015-12-10 12:28:35 -0600

    Add datetime for events and update views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -47,6 +47,6 @@ class EventsController < ApplicationController
 	private
 
 		def event_params
-			params.require(:event).permit(:title, :info)
+			params.require(:event).permit(:title, :info, :event_date_time)
 		end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151204050821) do
+ActiveRecord::Schema.define(version: 20151210175540) do
 
   create_table "chats", force: :cascade do |t|
     t.string   "user"
@@ -26,8 +26,9 @@ ActiveRecord::Schema.define(version: 20151204050821) do
   create_table "events", force: :cascade do |t|
     t.string   "title"
     t.text     "info"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.datetime "event_date_time"
   end
 
 end
